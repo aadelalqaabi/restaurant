994babb3d415e4b2d223e8f8d98b6551952a1890
aa0241 <77753344+aa0241@users.noreply.github.com>
2022-05-08 19:03:55 +0300

    DONE

---
+++ restaurant/src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1>The most delicious pizza in K-town</h1>
+      <p>
+        We serve create the pizza that you most crave. come to us and have a
+        slice!
+      </p>
+      <img
+        src="https://static.toiimg.com/thumb/53110049.cms?width=1200&height=900"
+        width={700}
+        height={700}
+      ></img>
+    </div>
+  );
+}
+
+export default App;
