1da8e3c90c30cb7104cacf6f43b922de34ec2faa
aa0241 <77753344+aa0241@users.noreply.github.com>
2022-05-08 19:51:55 +0300

    DONE

--- restaurant/src/App.js
+++ restaurant/src/App.js
@@ -1,18 +1,29 @@
 import "./App.css";
+import products from "./products";
 
 function App() {
   return (
     <div>
       <h1>The most delicious pizza in K-town</h1>
       <p>
-        We serve create the pizza that you most crave. come to us and have a
-        slice!
+        We serve the pizza that you most crave. Come to us and have a slice!
       </p>
       <img
         src="https://static.toiimg.com/thumb/53110049.cms?width=1200&height=900"
-        width={700}
-        height={700}
+        width={300}
+        height={300}
       ></img>
+      <tbody>
+        {products.map((product) => (
+          <tr key={product.name}>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+            <td>
+              <img src={product.imageURL} width={300} height={300}></img>
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </div>
   );
 }
---
+++ restaurant/src/products.js
@@ -0,0 +1,22 @@
+let products = [
+  {
+    name: "Chicago Pizza",
+    price: 4,
+    imageURL:
+      "https://pizzaneed.com/wp-content/uploads/2019/09/california-style-pizza.jpg",
+  },
+  {
+    name: "California Pizza",
+    price: 4,
+    imageURL:
+      "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/02ea0224-7316-406d-bb3d-5ed36e17a7b6/Derivates/85848d56-2911-4221-950f-b071d2d8fedf.jpg",
+  },
+  {
+    name: "K-town Pizza",
+    price: 2,
+    imageURL:
+      "https://www.budgetbytes.com/wp-content/uploads/2010/07/Classic-Homemade-Pizza-Dough-close.jpg",
+  },
+];
+
+export default products;
